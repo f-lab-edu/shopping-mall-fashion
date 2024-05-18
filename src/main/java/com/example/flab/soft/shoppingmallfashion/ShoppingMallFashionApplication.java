4d285e846e4c9a3475df93b1f4bcca8a46b02bd8
DoohyunHwang97 <ghkdengus97@gmail.com>
2024-05-18 16:28:33 +0900

    스프링 프로젝트 생성

---
+++ src/main/java/com/example/flab/soft/shoppingmallfashion/ShoppingMallFashionApplication.java
@@ -0,0 +1,13 @@
+package com.example.flab.soft.shoppingmallfashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallFashionApplication.class, args);
+	}
+
+}
