dd0400e3e25993b309d3ab558f1238cf2290dc90
DoohyunHwang97 <ghkdengus97@gmail.com>
2024-07-04 23:52:46 +0900

    test(Login, RefreshToken): 테스트 형식 수정 및 RefreshToken 테스트 추가

--- src/test/java/com/example/flab/soft/shoppingmallfashion/auth/LoginTest.java
+++ src/test/java/com/example/flab/soft/shoppingmallfashion/auth/LoginTest.java
@@ -1,13 +1,20 @@
 package com.example.flab.soft.shoppingmallfashion.auth;
 
+import com.example.flab.soft.shoppingmallfashion.auth.jwt.LoginRequest;
+import com.example.flab.soft.shoppingmallfashion.auth.jwt.TokenResponse;
+import com.example.flab.soft.shoppingmallfashion.common.SuccessResult;
+import com.example.flab.soft.shoppingmallfashion.user.domain.User;
+import com.example.flab.soft.shoppingmallfashion.user.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Map;
+import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.*;
 
 @Transactional
 @AutoConfigureMockMvc
@@ -23,32 +31,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class LoginTest {
     @Autowired
     private MockMvc mvc;
-
     @Autowired
     private ObjectMapper mapper;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    static final String EMAIL = "testUser@gmail.com";
+    static final String PASSWORD = "TestUser1#";
+    static final String WRONG_PASSWORD = "wrongPassword";
+
+    @BeforeEach
+    void beforeEach() {
+        userRepository.save(User.builder()
+                .id(1L)
+                .email(EMAIL)
+                .password(passwordEncoder.encode(PASSWORD))
+                .realName("testUser")
+                .cellphoneNumber("01012345678")
+                .nickname("testUser")
+                .createdAt(LocalDate.now())
+                .build());
+    }
 
     @Test
     @DisplayName("로그인 정보가 잘못되면 401 응답")
     void whenLoginWithWrongPassword_thenReturn401() throws Exception {
         mvc.perform(
-                        post("/api/v1/users/signup")
-                                .content(mapper.writeValueAsString(Map.of(
-                                        "email", "correct@gmail.com",
-                                        "password", "Correct1#",
-                                        "realName", "correct",
-                                        "cellphoneNumber", "01012345678",
-                                        "nickname", "correct"
-                                )))
-                                .contentType(MediaType.APPLICATION_JSON)
-                )
-                .andExpect(status().is(200));
-
-        mvc.perform(
                         post("/login")
-                                .content(mapper.writeValueAsString(Map.of(
-                                        "username", "correct@gmail.com",
-                                        "password", "wrongPassword"
-                                )))
+                                .content(mapper.writeValueAsString(LoginRequest.builder()
+                                        .username(EMAIL)
+                                        .password(WRONG_PASSWORD)
+                                        .build()))
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().is(401))
@@ -56,73 +71,55 @@ public class LoginTest {
     }
 
     @Test
-    @DisplayName("로그인 성공시 토큰 발금")
+    @DisplayName("로그인 성공시 토큰 발급")
     void whenLoginSuccess_thenReturnWithToken() throws Exception {
-        mvc.perform(
-                        post("/api/v1/users/signup")
-                                .content(mapper.writeValueAsString(Map.of(
-                                        "email", "correct@gmail.com",
-                                        "password", "Correct1#",
-                                        "realName", "correct",
-                                        "cellphoneNumber", "01012345678",
-                                        "nickname", "correct"
-                                )))
-                                .contentType(MediaType.APPLICATION_JSON)
-                )
-                .andExpect(status().is(200));
-
-        mvc.perform(
+        MvcResult mvcResult = mvc.perform(
                         post("/login")
-                                .content(mapper.writeValueAsString(Map.of(
-                                        "username", "correct@gmail.com",
-                                        "password", "Correct1#"
-                                )))
+                                .content(mapper.writeValueAsString(LoginRequest.builder()
+                                        .username(EMAIL)
+                                        .password(PASSWORD)
+                                        .build()))
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().is(200))
-                .andExpect(header().exists("Authorization"));
+                .andReturn();
+
+        SuccessResult<TokenResponse> result = mapper.readValue(mvcResult.getResponse().getContentAsString(),
+                mapper.getTypeFactory().constructParametricType(SuccessResult.class, TokenResponse.class));
+        TokenResponse tokenResponse = result.getResponse();
+
+        assertThat(tokenResponse).isNotNull();
+        assertThat(tokenResponse.getAccessToken()).isNotNull();
+        assertThat(tokenResponse.getRefreshToken()).isNotNull();
     }
 
     @Test
     @DisplayName("토큰이 있으면 401 에러가 발생하지 않는다")
     void whenHaveToken_thenReturn200() throws Exception {
-        mvc.perform(
-                        post("/api/v1/users/signup")
-                                .content(mapper.writeValueAsString(Map.of(
-                                        "email", "correct@gmail.com",
-                                        "password", "Correct1#",
-                                        "realName", "correct",
-                                        "cellphoneNumber", "01012345678",
-                                        "nickname", "correct"
-                                )))
-                                .contentType(MediaType.APPLICATION_JSON)
-                )
-                .andExpect(status().is(200));
-
         MvcResult mvcResult = mvc.perform(
                         post("/login")
-                                .content(mapper.writeValueAsString(Map.of(
-                                        "username", "correct@gmail.com",
-                                        "password", "Correct1#"
-                                )))
+                                .content(mapper.writeValueAsString(LoginRequest.builder()
+                                        .username(EMAIL)
+                                        .password(PASSWORD)
+                                        .build()))
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
-                .andExpect(status().is(200))
-                .andExpect(header().exists("Authorization"))
                 .andReturn();
 
-        String token = mvcResult.getResponse().getHeader("Authorization");
+        SuccessResult<TokenResponse> result = mapper.readValue(mvcResult.getResponse().getContentAsString(),
+                mapper.getTypeFactory().constructParametricType(SuccessResult.class, TokenResponse.class));
+        String accessToken = result.getResponse().getAccessToken();
 
         //without token
         mvc.perform(
-                        get("/")
+                        get("/api/v1/users/me")
                 )
                 .andExpect(status().is(401));
 
         //with token
         mvc.perform(
-                        get("/")
-                                .header("Authorization", token)
+                        get("/api/v1/users/me")
+                                .header("Authorization", "Bearer " + accessToken)
                 )
                 .andExpect(status().is(200));
     }
