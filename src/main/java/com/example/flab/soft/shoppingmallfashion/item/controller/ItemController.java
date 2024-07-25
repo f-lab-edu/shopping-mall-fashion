a4ffcb52b015a01949305852c0367708b58fcd7f
DoohyunHwang97 <ghkdengus97@gmail.com>
2024-07-25 16:19:57 +0900

    chore: 리뷰 반영, 도커 파일 추가

--- src/main/java/com/example/flab/soft/shoppingmallfashion/item/controller/ItemController.java
+++ src/main/java/com/example/flab/soft/shoppingmallfashion/item/controller/ItemController.java
@@ -1,7 +1,6 @@
 package com.example.flab.soft.shoppingmallfashion.item.controller;
 
 import com.example.flab.soft.shoppingmallfashion.common.SuccessResult;
-import com.example.flab.soft.shoppingmallfashion.item.domain.Sex;
 import com.example.flab.soft.shoppingmallfashion.item.service.ItemBriefDto;
 import com.example.flab.soft.shoppingmallfashion.item.service.ItemQueryService;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +10,6 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,12 +23,11 @@ public class ItemController {
             @PageableDefault(
                     size = 50, sort = "itemStats.orderCount",
                     direction = Direction.DESC) Pageable pageable,
-            @RequestParam(value = "minPrice", required = false) Integer minPrice,
-            @RequestParam(value = "maxPrice", required = false) Integer maxPrice,
-            @RequestParam(value = "categoryId", required = false) Long categoryId,
-            @RequestParam(value = "storeId", required = false) Long storeId,
-            @RequestParam(value = "sex", required = false) Sex sex) {
-        Page<ItemBriefDto> items = itemService.getItems(minPrice, maxPrice, categoryId, storeId, sex, pageable);
+            ItemListRequest itemListRequest) {
+        Page<ItemBriefDto> items = itemService.getItems(
+                itemListRequest.getMinPrice(), itemListRequest.getMaxPrice(),
+                itemListRequest.getCategoryId(), itemListRequest.getStoreId(),
+                itemListRequest.getSex(), pageable);
         return SuccessResult.<Page<ItemBriefDto>>builder().response(items).build();
     }
 }
