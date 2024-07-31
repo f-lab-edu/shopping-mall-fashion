166de976085aadecbc6592e886ee315ff4e37e19
DoohyunHwang97 <ghkdengus97@gmail.com>
2024-07-31 21:28:52 +0900

    test: DB락 분산락 수행시간 비교 추가

--- src/test/java/com/example/flab/soft/shoppingmallfashion/coupon/CouponServiceTest.java
+++ src/test/java/com/example/flab/soft/shoppingmallfashion/coupon/CouponServiceTest.java
@@ -5,11 +5,8 @@ import static org.assertj.core.api.Assertions.*;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.LongStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -29,8 +26,9 @@ class CouponServiceTest {
     private CouponRepository couponRepository;
     @Autowired
     private CouponRedissonRepository couponRedissonRepository;
-    private ExecutorService threads = Executors.newFixedThreadPool(10);
-    private CountDownLatch latch = new CountDownLatch(10);
+    private static final int THREAD_NUMBER = 100;
+    private ExecutorService threads = Executors.newFixedThreadPool(THREAD_NUMBER);
+    private CountDownLatch latch = new CountDownLatch(THREAD_NUMBER);
     private static final int ISSUED_AMOUNT = 100;
     @Autowired
     private PlatformTransactionManager txManager;
@@ -76,26 +74,25 @@ class CouponServiceTest {
 
     @Test
     @DisplayName("쿠폰 제공 동시성 테스트")
-    void couponRaceConditionTest() {
+    void couponRaceConditionTest() throws InterruptedException {
         TransactionTemplate txTemplate = new TransactionTemplate(txManager);
         couponService.issueCoupons("welcomeCoupon",
                 DiscountType.FIXED_PRICE_DISCOUNT,
-                1000, DiscountUnit.KRW, 10);
+                1000, DiscountUnit.KRW, THREAD_NUMBER);
         long startAt = System.currentTimeMillis();
-        List<Future<CouponInfo>> jobs = LongStream.range(1, 11)
-                .mapToObj(i -> threads.submit(() ->
-                    txTemplate.execute(status ->
-                            couponService.getCoupon(i, "welcomeCoupon"))))
-                .toList();
-
-        jobs.forEach(it -> {
-            try {
-                it.get();
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException(e);
-            }});
+        for (int i = 1; i < THREAD_NUMBER + 1; i++) {
+            long finalI = i;
+            threads.execute(() -> {
+                try {
+                    couponService.getCoupon(finalI, "welcomeCoupon");
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
         System.out.println("timeCost = " + (System.currentTimeMillis() - startAt));
-        List<Coupon> coupons = couponRepository.findAll();
+        List<Coupon> coupons = couponRedissonRepository.findAll();
         assertThat(coupons).allMatch(Coupon::hasOwner);
     }
 
@@ -105,9 +102,9 @@ class CouponServiceTest {
         TransactionTemplate txTemplate = new TransactionTemplate(txManager);
         couponService.issueCoupons("welcomeCoupon",
                 DiscountType.FIXED_PRICE_DISCOUNT,
-                1000, DiscountUnit.KRW, 10);
+                1000, DiscountUnit.KRW, THREAD_NUMBER);
         long startAt = System.currentTimeMillis();
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i < THREAD_NUMBER + 1; i++) {
             long finalI = i;
             threads.execute(() -> {
                 try {
