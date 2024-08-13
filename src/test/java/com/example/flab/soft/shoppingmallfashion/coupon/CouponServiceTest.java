1ef671e7bc5903f2599d2e2500aab04d98e10172
DoohyunHwang97 <ghkdengus97@gmail.com>
2024-08-13 19:58:00 +0900

    test: 쿠폰 테스트

--- src/test/java/com/example/flab/soft/shoppingmallfashion/coupon/CouponServiceTest.java
+++ src/test/java/com/example/flab/soft/shoppingmallfashion/coupon/CouponServiceTest.java
@@ -2,7 +2,18 @@ package com.example.flab.soft.shoppingmallfashion.coupon;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.time.LocalDateTime;
+import com.example.flab.soft.shoppingmallfashion.coupon.domain.Coupon;
+import com.example.flab.soft.shoppingmallfashion.coupon.domain.Discount;
+import com.example.flab.soft.shoppingmallfashion.coupon.domain.DiscountType;
+import com.example.flab.soft.shoppingmallfashion.coupon.domain.DiscountUnit;
+import com.example.flab.soft.shoppingmallfashion.coupon.domain.UserCoupon;
+import com.example.flab.soft.shoppingmallfashion.coupon.repository.CouponRepository;
+import com.example.flab.soft.shoppingmallfashion.coupon.repository.UserCouponRepository;
+import com.example.flab.soft.shoppingmallfashion.coupon.service.CouponInfo;
+import com.example.flab.soft.shoppingmallfashion.coupon.service.CouponRedissonService;
+import com.example.flab.soft.shoppingmallfashion.coupon.service.CouponService;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -24,30 +35,33 @@ class CouponServiceTest {
     @Autowired
     private CouponRepository couponRepository;
     @Autowired
-    private CouponRedissonRepository couponRedissonRepository;
-    private static final int THREAD_NUMBER = 100;
+    private UserCouponRepository userCouponRepository;
+    @Autowired
+    private static final int THREAD_NUMBER = 10;
     private ExecutorService threads = Executors.newFixedThreadPool(THREAD_NUMBER);
     private CountDownLatch latch = new CountDownLatch(THREAD_NUMBER);
-    private static final int ISSUED_AMOUNT = 100;
+    private static final long ISSUED_AMOUNT = 10;
     @Autowired
     private PlatformTransactionManager txManager;
 
     @AfterEach
     void tearDown() {
+        userCouponRepository.deleteAll();
         couponRepository.deleteAll();
-        couponRedissonRepository.deleteAll();
     }
 
     @Test
     @DisplayName("쿠폰 100개 발행")
     @Transactional
     void issueCoupons() {
-        Integer issuedCouponsCount = couponService.issueCoupons("welcomeCoupon",
+        Long couponId = couponService.issueCoupons("welcomeCoupon",
                 DiscountType.FIXED_PRICE_DISCOUNT,
-                1000, DiscountUnit.KRW, ISSUED_AMOUNT);
+                1000, DiscountUnit.KRW, ISSUED_AMOUNT,
+                3L, ChronoUnit.DAYS);
 
-        assertThat(issuedCouponsCount).isEqualTo(ISSUED_AMOUNT);
-        assertThat(couponRepository.count()).isEqualTo(ISSUED_AMOUNT);
+        Coupon coupon = couponRepository.findById(couponId).get();
+        assertThat(coupon.getAmounts()).isEqualTo(ISSUED_AMOUNT);
+        assertThat(couponRepository.count()).isEqualTo(1);
     }
 
     @Test
@@ -57,32 +71,41 @@ class CouponServiceTest {
         Long userId = 1L;
         Coupon coupon = couponRepository.save(Coupon.builder()
                 .name("welcomeCoupon")
+                .amounts(1L)
                 .discount(Discount.builder()
                         .discountType(DiscountType.FIXED_PRICE_DISCOUNT)
-                        .amount(1000)
+                        .discountAmount(1000)
                         .discountUnit(DiscountUnit.KRW)
                         .build())
-                .expiration(LocalDateTime.now().plusMonths(3))
+                .validation(Duration.of(3, ChronoUnit.DAYS))
                 .build());
 
-        CouponInfo couponInfo = couponService.getCoupon(userId, "welcomeCoupon");
-        assertThat(coupon.getOwnerId()).isEqualTo(userId);
-        assertThat(coupon.getOwned()).isTrue();
+        CouponInfo couponInfo = couponService.acquireCoupon(userId, coupon.getId());
+        List<UserCoupon> userCoupons = userCouponRepository.findByUserId(userId);
+        assertThat(userCoupons.size()).isEqualTo(1);
+        assertThat(userCoupons.get(0).getCoupon()).isEqualTo(coupon);
         assertThat(coupon.getName()).isEqualTo(couponInfo.getCouponName());
     }
 
     @Test
     @DisplayName("쿠폰 제공 동시성 테스트")
     void couponRaceConditionTest() throws InterruptedException {
-        couponService.issueCoupons("welcomeCoupon",
-                DiscountType.FIXED_PRICE_DISCOUNT,
-                1000, DiscountUnit.KRW, THREAD_NUMBER);
+        Coupon coupon = couponRepository.save(Coupon.builder()
+                .name("welcomeCoupon")
+                .amounts((long) THREAD_NUMBER)
+                .discount(Discount.builder()
+                        .discountType(DiscountType.FIXED_PRICE_DISCOUNT)
+                        .discountAmount(1000)
+                        .discountUnit(DiscountUnit.KRW)
+                        .build())
+                .validation(Duration.of(3, ChronoUnit.DAYS))
+                .build());
         long startAt = System.currentTimeMillis();
         for (int i = 1; i < THREAD_NUMBER + 1; i++) {
-            long finalI = i;
+            long userId = i;
             threads.execute(() -> {
                 try {
-                    couponService.getCoupon(finalI, "welcomeCoupon");
+                    couponService.acquireCoupon(userId, coupon.getId());
                 } finally {
                     latch.countDown();
                 }
@@ -90,22 +113,29 @@ class CouponServiceTest {
         }
         latch.await();
         System.out.println("timeCost = " + (System.currentTimeMillis() - startAt));
-        List<Coupon> coupons = couponRedissonRepository.findAll();
-        assertThat(coupons).allMatch(Coupon::hasOwner);
+        assertThat(userCouponRepository.count()).isEqualTo(THREAD_NUMBER);
+        assertThat(couponRepository.findById(coupon.getId()).get().getAmounts()).isEqualTo(0);
     }
 
     @Test
     @DisplayName("Redisson 이용한 쿠폰 제공 동시성 테스트")
     void couponRaceConditionRedissonTest() throws InterruptedException {
-        couponService.issueCoupons("welcomeCoupon",
-                DiscountType.FIXED_PRICE_DISCOUNT,
-                1000, DiscountUnit.KRW, THREAD_NUMBER);
+        Coupon coupon = couponRepository.save(Coupon.builder()
+                .name("welcomeCoupon")
+                .amounts((long) THREAD_NUMBER)
+                .discount(Discount.builder()
+                        .discountType(DiscountType.FIXED_PRICE_DISCOUNT)
+                        .discountAmount(1000)
+                        .discountUnit(DiscountUnit.KRW)
+                        .build())
+                .validation(Duration.of(3, ChronoUnit.DAYS))
+                .build());
         long startAt = System.currentTimeMillis();
         for (int i = 1; i < THREAD_NUMBER + 1; i++) {
-            long finalI = i;
+            long userId = i;
             threads.execute(() -> {
                 try {
-                    couponRedissonService.getCoupon(finalI, "welcomeCoupon");
+                    couponRedissonService.getCoupon(userId, coupon.getId());
                 } finally {
                     latch.countDown();
                 }
@@ -113,7 +143,7 @@ class CouponServiceTest {
         }
         latch.await();
         System.out.println("timeCost = " + (System.currentTimeMillis() - startAt));
-        List<Coupon> coupons = couponRedissonRepository.findAll();
-        assertThat(coupons).allMatch(Coupon::hasOwner);
+        assertThat(userCouponRepository.count()).isEqualTo(THREAD_NUMBER);
+        assertThat(couponRepository.findById(coupon.getId()).get().getAmounts()).isEqualTo(0);
     }
 }
